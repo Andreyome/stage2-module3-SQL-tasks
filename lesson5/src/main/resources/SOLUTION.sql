37e8e8a6c27ffd30c91bdbaa5145c500bb7313e4
Andreyome <118749442+Andreyome@users.noreply.github.com>
2023-03-24 01:10:31 +0200

    Update SOLUTION.sql

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -2,6 +2,6 @@ SELECT * FROM payment WHERE amount>=500;
 SELECT * FROM student WHERE birthday< DATEADD(YEAR,-20,CURDATE());
 SELECT * FROM student WHERE birthday> DATEADD(YEAR,-20,CURDATE()) AND groupnumber=10;
 SELECT * FROM student WHERE groupnumber IN (4, 5, 6) OR name LIKE 'Mike%';
-SELECT * FROM payment WHERE DATEADD(month,-8,'2023-03-01') < payment_date ;
+SELECT * FROM payment WHERE DATEADD(month,-8,'2023-02-01') < payment_date ;
 SELECT * FROM student WHERE name LIKE 'A%';
 SELECT * FROM student WHERE (name LIKE 'Roxi%' AND groupnumber =4) OR (name LIKE 'Tallie%' AND groupnumber = 9);
