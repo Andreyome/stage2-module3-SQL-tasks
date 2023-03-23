7a6275b852acde1c4bdce9a9a970331d90f10227
Andreyome <118749442+Andreyome@users.noreply.github.com>
2023-03-24 00:39:34 +0200

    Update SOLUTION.sql

--- lesson7/src/main/resources/SOLUTION.sql
+++ lesson7/src/main/resources/SOLUTION.sql
@@ -1,5 +1,5 @@
 SELECT * FROM mark WHERE mark > 6 ORDER BY mark DESC;
 SELECT * FROM payment WHERE amount < 300 ORDER BY amount ASC;
 SELECT * FROM paymenttype ORDER BY name;
-SELECT * FROM student ORDER BY DESC;
+SELECT * FROM student ORDER BY name DESC;
 SELECT DISTINCT * FROM student WHERE id IN (SELECT student_id FROM payment WHERE amount >1000) ORDER BY birthday;
