74d061a38ae552721d14a172126e612b01d66141
Andreyome <118749442+Andreyome@users.noreply.github.com>
2023-03-23 23:13:37 +0200

    Update SOLUTION.sql

--- lesson2/src/main/resources/SOLUTION.sql
+++ lesson2/src/main/resources/SOLUTION.sql
@@ -17,9 +17,9 @@ INSERT INTO SUBJECT ( name, grade)  values (  'Science', 4 );
 INSERT INTO SUBJECT ( name, grade)  values (  'Math', 5);
 INSERT INTO SUBJECT ( name, grade)  values (  'History', 5 );
 
-INSERT INTO PAYMENTTYPE (ID, NAME) values ( 'DAILY');
-INSERT INTO PAYMENTTYPE (ID, NAME) values (  'WEEKLY' );
-INSERT INTO PAYMENTTYPE (ID, NAME) values (  'MONTHLY' );
+INSERT INTO PAYMENTTYPE (NAME) values ( 'DAILY');
+INSERT INTO PAYMENTTYPE ( NAME) values (  'WEEKLY' );
+INSERT INTO PAYMENTTYPE ( NAME) values (  'MONTHLY' );
 
 INSERT INTO PAYMENT (TYPE_ID, AMOUNT, PAYMENT_DATE, STUDENT_ID) VALUES ( 2, 50, '2013-01-03' ,  1);
 INSERT INTO PAYMENT (TYPE_ID, AMOUNT, PAYMENT_DATE, STUDENT_ID) VALUES ( 3, 50, '2013-01-03' ,  4);
@@ -27,12 +27,12 @@ INSERT INTO PAYMENT (TYPE_ID, AMOUNT, PAYMENT_DATE, STUDENT_ID) VALUES ( 2, 50, 
 INSERT INTO PAYMENT (TYPE_ID, AMOUNT, PAYMENT_DATE, STUDENT_ID) VALUES ( 1, 50, '2013-01-03' ,  5);
 
 
-insert into MARK ( SUBJECT_ID, MARK) values (  8);
-insert into MARK ( SUBJECT_ID, MARK) values (  5);
-insert into MARK ( SUBJECT_ID, MARK) values (  9);
-insert into MARK ( SUBJECT_ID, MARK) values (  4);
-insert into MARK ( SUBJECT_ID, MARK) values (  9);
-insert into MARK ( SUBJECT_ID, MARK) values (  6);
-insert into MARK ( SUBJECT_ID, MARK) values (  2);
+insert into MARK (student_id, SUBJECT_ID, MARK) values (2, 1, 8);
+insert into MARK (student_id, SUBJECT_ID, MARK) values (4, 4,  5);
+insert into MARK (student_id, SUBJECT_ID, MARK) values (5, 3,  9);
+insert into MARK (student_id, SUBJECT_ID, MARK) values (8, 6,  4);
+insert into MARK (student_id, SUBJECT_ID, MARK) values (9, 5,  9);
+insert into MARK (student_id, SUBJECT_ID, MARK) values (1, 8,  6);
+insert into MARK (student_id, SUBJECT_ID, MARK) values (7, 7,  2);
 
 select (s.NAME, P3.NAME) from STUDENT s inner join PAYMENT P2 on s.ID = P2.STUDENT_ID inner join PAYMENTTYPE P3 on P3.ID = P2.TYPE_ID;
